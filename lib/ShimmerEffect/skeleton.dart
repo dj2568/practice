351eb4179f77d90cdfeb5cabbcbee4d32199a3cb
dj2568 <dj2568@gmail.com>
2023-01-10 16:21:00 +0530

    10/01/2023

--- lib/ShimmerEffect/skeleton.dart
+++ lib/ShimmerEffect/skeleton.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const _shimmerGradient = LinearGradient(
+const shimmerGradient = LinearGradient(
   colors: [
     Color(0xFFEBEBF4),
     Color(0xFFF4F4F4),
